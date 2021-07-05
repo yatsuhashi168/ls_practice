a8a38315315dc8c9ad9f3dac6cef1690026cea2f
yatsuhashi168 <kurimi777luk@gmail.com>
2021-07-05 15:43:31 +0900

    -a,-rオプションの処理を記述、提出用コード作成

--- ls.rb
+++ ls.rb
@@ -1,12 +1,31 @@
+#!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
 require 'etc'
 
-files = Dir.glob('*', sort: true)
-# ここで-a, -rオプションの処理
-p files
+def main
+  options = { a: false, l: false, r: false }
+  opt = OptionParser.new
+  opt.on('-a', 'show hidden files') { options[:a] = true }
+  opt.on('-l', 'show details') { options[:l] = true }
+  opt.on('-r', 'reverse the order') { options[:r] = true }
 
-# ここで`-l`オプションの処理
+  opt.parse!(ARGV)
+  files = if options[:a]
+            Dir.glob('*', File::FNM_DOTMATCH, sort: true)
+          else
+            Dir.glob('*', sort: true)
+          end
+
+  files = files.reverse if options[:r]
+
+  if options[:l]
+    output_with_l_option(files)
+  else
+    output(files)
+  end
+end
 
 def filetype(filemode)
   {
@@ -28,56 +47,53 @@ def permission(filemode)
   }[filemode]
 end
 
-puts "total #{files.inject(0) { |total, file| total + File.stat(file).blocks }}"
-files.each do |file|
-  fileinfo = File.stat(file)
-  filemode = fileinfo.mode.to_s(8)
-  print filetype(filemode[0, 2])
-  print permission(filemode[3])
-  print permission(filemode[4])
-  print permission(filemode[5])
-  print "#{permission(filemode[6])} "
-  print fileinfo.nlink
-  print "#{Etc.getpwuid(fileinfo.uid).name} "
-  print "#{fileinfo.size} "
-  print "#{fileinfo.mtime.strftime('%b %e %R')} "
-  print file
-  puts
+def output_with_l_option(files)
+  puts "total #{files.inject(0) { |total, file| total + File.stat(file).blocks }}"
+  files.each do |file|
+    fileinfo = File.stat(file)
+    filemode = fileinfo.mode.to_s(8)
+    print filetype(filemode[0, 2])
+    print permission(filemode[3])
+    print permission(filemode[4])
+    print permission(filemode[5])
+    print "#{permission(filemode[6])} "
+    print "#{fileinfo.nlink} "
+    print "#{Etc.getpwuid(fileinfo.uid).name} "
+    print "#{Etc.getgrgid(fileinfo.gid).name} "
+    print "#{fileinfo.size} "
+    print "#{fileinfo.mtime.strftime('%b %e %R')} "
+    print file
+    puts
+  end
 end
 
-# line = if (files.size % 3).zero?
-#          files.size / 3
-#        else
-#          files.size / 3 + 1
-#        end
+def lined_up(files)
+  line = if (files.size % 3).zero?
+           files.size / 3
+         else
+           files.size / 3 + 1
+         end
 
-# lined_up_files = []
-# if files.size == 4
-#   lined_up_files << [files[0], files[1]]
-#   lined_up_files << [files[2]]
-#   lined_up_files << [files[3]]
-# else
-#   files.each_slice(line) do |n|
-#     lined_up_files << n
-#   end
-# end
-# max_size = lined_up_files.map(&:size).max
-# lined_up_files.map! { |it| it.values_at(0...max_size) }
-#
-# width = files.max_by(&:size).size
-# lined_up_files.transpose.each do |lined_up_file|
-#   lined_up_file.each do |n|
-#     print n.ljust(width + 3) unless n.nil?
-#   end
-#   puts
-# end
+  lined_up_files = []
+  files.each_slice(line) do |n|
+    lined_up_files << n
+  end
 
-# p h = (1..files.size).to_a.group_by{|i| i % line}
+  max_size = lined_up_files.map(&:size).max
+  lined_up_files.map! { |it| it.values_at(0...max_size) }
+  lined_up_files
+end
 
-# subscript = 0
-# files.size.times do |n|
-#   print files[subscript].ljust(20)
-#   if subscript >= files.size
-#   subscript += line
-#
-# end
+def output(files)
+  lined_up_files = lined_up(files)
+
+  width = files.max_by(&:size).size
+  lined_up_files.transpose.each do |lined_up_file|
+    lined_up_file.each do |n|
+      print n.ljust(width + 3) unless n.nil?
+    end
+    puts
+  end
+end
+
+main
